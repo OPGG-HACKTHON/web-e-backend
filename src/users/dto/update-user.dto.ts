4e6102c5c6e73dd86ed3c61d751b861bd99a0f52
rnin9 <rkdalswn1209@kw.ac.kr>
2021-07-20 20:51:34 +0900

    make develop branch

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
 import { CreateUserDto } from './create-user.dto';
 import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {} //선택적으로 만들기 가능
+export class UpdateUserDto extends PartialType(CreateUserDto) {} //선택적으로 만들기 가능함
