31c83e0d4de270da92aa1524e08c7797e99aeab0
jKroll87 <bingquadog@gmail.com>
2021-07-25 16:02:20 +0900

    Implement Videos resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { VideosModule } from './videos/videos.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, VideosModule],
   controllers: [AppController],
   providers: [],
 })
