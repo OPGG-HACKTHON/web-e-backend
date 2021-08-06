83b699c7617a132d4009ef03a7db45ee72badb8f
jKroll87 <bingquadog@gmail.com>
2021-08-06 19:45:07 +0900

    Add videos.module

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { VideosService } from './videos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Video } from './entities/video.entity';
+import { AuthModule } from 'src/auth/auth.module';
 import { VideosController } from './videos.controller';
+import { VideosService } from './videos.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Video]), AuthModule],
   controllers: [VideosController],
-  providers: [VideosService]
+  providers: [VideosService],
+  exports: [VideosService]
 })
 export class VideosModule {}
