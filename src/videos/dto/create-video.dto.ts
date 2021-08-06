edfbdb191694d7229a70ae8558e6738a5638d620
jKroll87 <bingquadog@gmail.com>
2021-08-06 19:45:41 +0900

    Add video dto

--- src/videos/dto/create-video.dto.ts
+++ src/videos/dto/create-video.dto.ts
@@ -1,1 +1,31 @@
-export class CreateVideoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, IsEnum } from 'class-validator';
+
+enum Game {
+  League_of_Legends,
+  Overwatch,
+  PUBG_BATTLEGROUNDS
+}
+
+export class CreateVideoDto {
+  @IsString()
+  @ApiProperty({ type: String, description: '사용자 ID' })
+  readonly userId: string;
+
+  @IsString()
+  @ApiProperty({ type: String, description: '동영상 이름' })
+  readonly name: string;
+
+  @IsEnum(Game)
+  @ApiProperty({ type: Number, description: '동영상 게임' })
+  readonly game: number;
+
+  @IsString()
+  @ApiProperty({ type: String, description: '동영상 url' })
+  readonly url: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ type: String, description: '동영상 설명' })
+  readonly description: string;
+}
--- src/videos/dto/update-video.dto.ts
+++ src/videos/dto/update-video.dto.ts
@@ -1,4 +1,35 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateVideoDto } from './create-video.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, IsEnum } from 'class-validator';
 
-export class UpdateVideoDto extends PartialType(CreateVideoDto) {}
+enum Game {
+  League_of_Legends,
+  Overwatch,
+  PUBG_BATTLEGROUNDS
+}
+
+export class UpdateVideoDto {
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ type: String, description: '사용자 ID' })
+  readonly userId: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ type: String, description: '동영상 이름' })
+  readonly name: string;
+
+  @IsEnum(Game)
+  @IsOptional()
+  @ApiProperty({ type: Number, description: '동영상 게임' })
+  readonly game: number ;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ type: String, description: '동영상 url' })
+  readonly url: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ type: String, description: '동영상 설명' })
+  readonly description: string;
+}
