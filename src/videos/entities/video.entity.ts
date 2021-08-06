71d587de9b00cb3d764ce9cae23b092601a3f0c6
jKroll87 <bingquadog@gmail.com>
2021-08-06 19:45:20 +0900

    Add video entity

--- src/videos/entities/video.entity.ts
+++ src/videos/entities/video.entity.ts
@@ -1,1 +1,55 @@
-export class Video {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { IsDate, IsEnum, IsNumber, IsString } from 'class-validator';
+
+enum Game {
+  League_of_Legends,
+  Overwatch,
+  PUBG_BATTLEGROUNDS
+}
+
+@Entity()
+export class Video {
+  @IsNumber()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsString()
+  @Column('varchar')
+  userId: string;
+
+  @IsDate()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @IsDate()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @IsString()
+  @Column()
+  name: string;
+
+  @IsEnum(Game)
+  @Column({ type: 'enum', enum: Game })
+  game: number;
+
+  @IsString()
+  @Column('varchar')
+  url: string;
+
+  @IsString()
+  @Column({ type: 'varchar', default: "" })
+  description: string;
+
+  @IsNumber()
+  @Column({ type: 'int', default: 0 })
+  comments: number;
+
+  @IsNumber()
+  @Column({ type: 'int', default: 0 })
+  views: number;
+
+  @IsNumber()
+  @Column({ type: 'int', default: 0 })
+  likes: number;
+}
