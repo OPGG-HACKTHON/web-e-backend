46f74483d941712ca91d1d12f477207f8b5eb0f1
jKroll87 <bingquadog@gmail.com>
2021-08-07 19:47:38 +0900

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('WATPL APIs')
