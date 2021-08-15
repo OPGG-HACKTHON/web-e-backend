c007ea3a4bd3f7b7340ec0c3ee9a97f1fd3ff392
jKroll87 <bingquadog@gmail.com>
2021-08-15 15:34:10 +0900

    Inject follow dependency in video module

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Video } from './entities/video.entity';
+import { Follow } from 'src/follow/entities/follow.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { VideosController } from './videos.controller';
 import { VideosService } from './videos.service';
@@ -10,6 +11,7 @@ import { VideosService } from './videos.service';
   imports: [
     TypeOrmModule.forFeature([User]),
     TypeOrmModule.forFeature([Video]),
+    TypeOrmModule.forFeature([Follow]),
     AuthModule,
   ],
   controllers: [VideosController],
