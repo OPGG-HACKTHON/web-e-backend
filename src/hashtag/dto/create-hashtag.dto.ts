7651c0e37d9b268d473b097a822dcc58f25bbb15
jKroll87 <bingquadog@gmail.com>
2021-08-18 04:47:53 +0900

    Add hashtag dto

--- src/hashtag/dto/create-hashtag.dto.ts
+++ src/hashtag/dto/create-hashtag.dto.ts
@@ -1,1 +1,14 @@
-export class CreateHashtagDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateHashtagDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, description: '해시태그' })
+  readonly hashtag: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, description: '게임 카테고리' })
+  readonly category: string;
+}
