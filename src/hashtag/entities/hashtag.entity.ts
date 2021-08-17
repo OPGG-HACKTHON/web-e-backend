560f16e33f6aa8a6673d84442cf49a5fde856b9f
jKroll87 <bingquadog@gmail.com>
2021-08-17 19:53:43 +0900

    Add category column to hashtag entity

--- src/hashtag/entities/hashtag.entity.ts
+++ src/hashtag/entities/hashtag.entity.ts
@@ -1,9 +1,13 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Game } from 'src/videos/enums/game'; // Modify later
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Hashtag {
   @PrimaryColumn('varchar')
   hashtag: string;
+
+  @Column({ type: 'enum', enum: Game })
+  category: string;
 
   @Column({ type: 'int', default: 0 })
   cnt: number;
